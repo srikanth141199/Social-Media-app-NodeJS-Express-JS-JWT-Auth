7e66fe62dd447db4bb93d2eb02bdc3f8d4260f59
Srikanth K <kolleparasrikanth@gmail.com>
2024-03-09 15:22:51 +0530

    error handlers

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import userRouter from './src/routes/user.routes.js';
 import postsRouter from './src/routes/posts.routes.js';
 import commentsRouter from './src/routes/comments.routes.js';
 import likesRouter from './src/routes/likes.routes.js';
+import { ApplicationError } from './src/error-handler/applicationError.js';
 
 
 
@@ -48,6 +49,28 @@ app.use("/api/user", userRouter);
 app.use("/api/posts", postsRouter);
 app.use("/api/comments", commentsRouter);
 app.use("/api/likes", likesRouter);
+
+// Error handler middleware
+app.use((err, req, res, next)=>{
+  console.log(err);
+  if (err instanceof ApplicationError){
+    res.status(err.code).send(err.message);
+  }
+
+  // server errors.
+  res
+  .status(500)
+  .send(
+    'Something went wrong, please try later'
+    );
+});
+
+// 4. Middleware to handle 404 requests.
+app.use((req, res)=>{
+  res.status(404).send("API not found. Please check our documentation for more information at localhost:3200/api-docs")
+});
+
+
 //Server
 app.listen(5100, () => {
     console.log('Server is running on port 5100');
